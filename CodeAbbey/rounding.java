da0a7e8c744147a8da4e038d90c2acc207072e87
Sydnee Wren <swren1@student.gsu.edu>
2015-04-19 11:28:04 -0400

    Create rounding.java

---
+++ CodeAbbey/rounding.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+
+public class rounding{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        int cases = in.nextInt();
+        
+        for (int i = 0; i < cases; i++){
+            double a = in.nextInt();
+            double b = in.nextInt();
+        
+            System.out.print(Math.round(a/b) + " ");
+        }
+        
+    }
+}
