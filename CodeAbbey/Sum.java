2312958ec8f2a79c2705f4e696ca64eb311e437a
Sydnee Wren <swren1@student.gsu.edu>
2015-04-19 10:26:24 -0400

    Update Sum.java

--- CodeAbbey/Sum.java
+++ CodeAbbey/Sum.java
@@ -1,12 +1,11 @@
-import java.util.*;//import Scanner
+//Problem #1
+import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-       
-        int a, b;
-        a = in.nextInt();
-        b = in.nextInt();
+        int  a = in.nextInt();
+        int b = in.nextInt();
         
         System.out.println(a + b);
     }
