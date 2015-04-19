41e0d2c8a42bb2c85b4e6faebff7f93920a344a0
Sydnee Wren <swren1@student.gsu.edu>
2015-04-19 10:09:26 -0400

    Update sumsInLoop.java
    Reduced a unused variable and reduced some redundancy.

--- CodeAbbey/sumsInLoop.java
+++ CodeAbbey/sumsInLoop.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-public class sumsInLoops{
+public class sumInLoop{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-    
+        
         int cases = in.nextInt();
         int sum = 0;
-    
+        
         for (int i = 0; i < cases; i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            System.out.print(a + b + " ");
+            sum += in.nextInt();
         }
+        System.out.println(sum);
     }
 }
