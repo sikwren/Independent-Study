d9e0838a68436226b1ffe8e38e5a66bf92e7056e
Sydnee Wren <swren1@student.gsu.edu>
2015-04-19 10:24:09 -0400

    Update sumsInLoop.java

--- CodeAbbey/sumsInLoop.java
+++ CodeAbbey/sumsInLoop.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class sumInLoop{
+public class sumsInLoop{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         
         int cases = in.nextInt();
-        int sum = 0;
         
         for (int i = 0; i < cases; i++){
-            sum += in.nextInt();
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.print(a + b + " ");
         }
-        System.out.println(sum);
     }
 }
